2d21860c5d0ae534fbaea4cd816eecd05c13dd34
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2023-05-10 15:35:19 +0900

    saveInstance, readInstance

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,74 @@
 import 'dart:convert'; // JSON 인코딩 및 디코딩을 위한 라이브러리
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // 로컬 데이터 저장을 위한 라이브러리
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: MyDataWidget()); // 앱의 루트 위젯 설정
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyDataWidget extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyDataWidgetState createState() =>
+      _MyDataWidgetState(); // 상태를 가지는 위젯의 상태 클래스 반환
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyDataWidgetState extends State<MyDataWidget> {
+  MyData? _data; // 사용자 데이터 저장을 위한 변수
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _loadData(); // 사용자 데이터 로드
+  }
+
+  Future<void> _loadData() async {
+    MyData? loadedData = await MyData.readInstance(); // 저장된 데이터 불러오기
+    print(loadedData?.toJson()); // 로그 출력
+    if (loadedData != null) {
+      setState(() {
+        _data = loadedData; // 로드된 데이터로 상태 업데이트
+      });
+    } else {
+      setState(() {
+        _data = MyData(name: 'my name', age: 1); // 초기 데이터 생성
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('SharedPreferences Demo'), // 상단바 제목 설정
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: _data == null
+          ? CircularProgressIndicator() // 데이터 로딩 중일 때 로딩 아이콘 출력
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Name: ${_data!.name}'), // 사용자 이름 출력
+                  Text('Age: ${_data!.age}'), // 사용자 나이 출력
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () async {
+          setState(() {
+            _data!.age = _data!.age + 1; // 나이 증가
+          });
+          await _data?.saveInstance(); // 데이터 저장
+        },
+        tooltip: 'saveInstance',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
